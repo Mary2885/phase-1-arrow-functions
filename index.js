9b4821cda68ff3cbcdaef72c34537aab717ef452
mary <Mariashee001@gmail.com>
2024-03-21 11:08:19 +0300

    complete arrow function

---
+++ index.js
@@ -0,0 +1,20 @@
+// Define the divide function expression
+let divide = function() {
+    return 2000 / 100;
+  }
+  
+  // Define the square arrow function
+  let square = (num) => {
+    return num * num;
+  }
+  
+  // Define the add arrow function
+  let add = (num1, num2) => {
+    return num1 + num2;
+  }
+  
+  // Test the defined functions
+  console.log(divide()); // Output: 20
+  console.log(square(4)); // Output: 16
+  console.log(add(5, 10)); // Output: 15
+  
